d0c8d84e8866849c5100c0de36a6dab6ca9aae50
MaTiUs77 <matius77@gmail.com>
2018-05-30 19:40:31 -0300

    Cambio de dependencias
    Primer test funcional

--- app/server.js
+++ app/server.js
@@ -1,9 +1,33 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
+// include dependencies
+var express = require('express');
+var proxy = require('http-proxy-middleware');
 
-var port = 80;
-var server = httpProxy.createProxyServer({target:'http://172.17.0.1:8888'});
+var proxyPort = 80;
 
-console.log("siep-http-proxy listening on port", port)
-server.listen(port);
- 
+// proxys
+var auth_api = proxy({
+       target: 'http://172.17.0.1:8888',
+       changeOrigin: true,
+       pathRewrite: {
+            '^/auth' : '/',
+       }
+    }
+);
+
+var laravel_api = proxy({
+       target: 'http://172.17.0.1:3000',
+       changeOrigin: true,
+       pathRewrite: {
+            '^/api' : '/api',
+       }
+    }
+);
+
+// mount `exampleProxy` in web server
+var app = express();
+
+app.use('/auth/**', auth_api);
+app.use('/api/**', laravel_api);
+    
+console.log("proxy-port listening on port",proxyPort);
+app.listen(proxyPort);
