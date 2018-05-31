1a2b78ca4d21be809a57143a9c306292c4b4e941
MaTiUs77 <matius77@gmail.com>
2018-05-31 12:49:43 -0300

    github info desde consola

---
+++ app/config.js
@@ -0,0 +1,14 @@
+var config = {};
+
+config.port = 80;
+config.index = {
+    'service': 'node-http-proxy',
+    'status': 'online',
+    'motor': 'NodeJS',
+    'github': {
+       'url': null,
+       'tag': null
+    }
+};
+
+module.exports = config;
--- app/server.js
+++ app/server.js
@@ -1,33 +1,49 @@
 // include dependencies
 var express = require('express');
 var proxy = require('http-proxy-middleware');
+var cmd = require('child_process');
 
-var proxyPort = 80;
-
-// proxys
-var auth_api = proxy({
-       target: 'http://172.17.0.1:8888',
-       changeOrigin: true,
-       pathRewrite: {
-            '^/auth' : '/',
-       }
-    }
-);
-
-var laravel_api = proxy({
-       target: 'http://172.17.0.1:3000',
-       changeOrigin: true,
-       pathRewrite: {
-            '^/api' : '/api',
-       }
-    }
-);
-
-// mount `exampleProxy` in web server
+var config = require('./config');
+var routes = require('./routes');
 var app = express();
 
-app.use('/auth/**', auth_api);
-app.use('/api/**', laravel_api);
-    
-console.log("proxy-port listening on port",proxyPort);
-app.listen(proxyPort);
+function execPromise(command) {
+    return new Promise(function(resolve, reject) {
+        cmd.exec(command, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            resolve(stdout.trim());
+        });
+    });
+}
+
+app.get('/', async function (req, res) {
+  try {
+      let origin = await execPromise('git remote -v');
+      origin = origin.replace('origin','').replace('\t','');
+      origin = origin.split(" ")[0];
+      config.index.github.url = origin;
+      config.index.github.tag  = await execPromise('git describe --always --tags');
+  } catch (e) {
+      console.error(e.message);
+  }
+
+    res.json(config.index);
+});
+
+routes.forEach(function(gw) {
+  let route_path= {};
+  route_path[`^${gw.path}`] = gw.target_path;
+
+  app.use(`${gw.path}/**`, proxy({
+    changeOrigin: true,
+    target: gw.target,
+    pathRewrite: route_path
+  }));
+});
+
+console.log("proxy-port listening on port",config.port);
+app.listen(config.port);
