5c785a36ef0865e5a3c39fdf6e346b22a0109001
MaTiUs77 <matius77@gmail.com>
2018-06-07 02:00:45 -0300

    Environment para desarrollo

--- app/routes.js
+++
@@ -1,6 +0,0 @@
-var routes = [
-  { target: 'http://172.17.0.1:3000', path: '/api', target_path: '/api' },
-  { target: 'http://172.17.0.1:8888', path: '/auth', target_path: '/' }
-];
-
-module.exports = routes;
---
+++ docker_dev/app/routes.js
@@ -0,0 +1,6 @@
+var routes = [
+  { target: 'http://siep-api-web', path: '/api', target_path: '/api' },
+  { target: 'http://siep-auth-web', path: '/auth', target_path: '/' }
+];
+
+module.exports = routes;
